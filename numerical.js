65908371775ecef54b84a814892d0468b040b970
jgoecks <jgoecks@gwu.edu>
2016-04-14 21:56:27 -0400

    Add histogram display mode and refactor to clean up code.

--- numerical.js
+++ numerical.js
@@ -4,85 +4,6 @@
 define(["jquery", "d3.min", "vega"], function($, d3, vega) {
 
     /**
-     * Draw a line chart with one or more data sources.
-     */
-    var drawLineChart = function(dataList, options) {
-        options = options || {};
-
-        // Set up chart-specific operations.
-        var marksTemplate = "",
-            marksFunc,
-            dataConversionFunc;
-        if (options.type === "line") {
-            marksTemplate = "./chart_defs/line_marks.json";
-            marksFunc = function(marksSpec, color) {
-                marksSpec.properties.update.stroke.value = color;
-            };
-            dataConversionFunc = convertForLineOrFill;
-        }
-        else if (options.type === "fill") {
-            marksTemplate = "./chart_defs/fill_marks.json";
-            marksFunc = function(marksSpec, color) {
-                marksSpec.properties.update.fill.value = color;
-            };
-            dataConversionFunc = convertForLineOrFill;
-        }
-        else if (options.type === "intensity") {
-            marksTemplate = "./chart_defs/intensity_marks.json";
-            marksFunc = function() {};
-            dataConversionFunc = convertForIntensity;
-        }
-
-
-        // Load chart definition, update, and display.
-        $.getJSON("./chart_defs/base.json", function(vizSpec) {
-            $.getJSON(marksTemplate, function(marksSpec) {
-
-                // Set chart attributes from options.
-                if (options.ydomain) {
-                    vizSpec.scales[1].domain = options.ydomain;
-                }
-
-                // Draw each dataset.
-                var yMax = 0;
-                for (var i = 0; i < dataList.length; i++) {
-                    var dataset = dataList[i],
-                        name = dataset.name,
-                        data = dataset.data,
-                        color = dataset.color,
-                        dataMarksSpec = $.extend(true, {}, marksSpec);
-
-                    // Convert features to datapoints. Each data point may be for a single
-                    // base or multiple bases.
-                    converted_data = dataConversionFunc(data);
-
-                    // Compute 90% quantile and set y max to that if its larger.
-                    dataSetMax = d3.quantile($.map(data, function(d) { return d.score; }), 0.95);
-                    if (dataSetMax > yMax) {
-                        yMax = dataSetMax;
-                    }
-
-                    dataMarksSpec.from.data = name;
-                    marksFunc(dataMarksSpec, color);
-                    vizSpec.marks.push(dataMarksSpec);
-                    vizSpec.data.push({
-                        "name": name,
-                        "values": converted_data
-                    });
-                }
-
-                // Set yMax, multiplying by 1.5 to put maximum in middle of chart.
-                vizSpec.scales[1].domain = [0, yMax * 1.5];
-                vega.parse.spec(vizSpec, function(chart) {
-                    chart({el:"#vis"}).update();
-                });
-            });
-
-
-        });
-    };
-
-    /**
      * Convert data for line/fill display.
      */
     var convertForLineOrFill = function(data) {
@@ -127,6 +48,93 @@ define(["jquery", "d3.min", "vega"], function($, d3, vega) {
                     score: interval.score
                 };
             }
+        });
+    };
+
+    /**
+     * Attributes for each chart type.
+     */
+    var chartTypeDicts = {
+        "line": {
+            marksSpec: "./chart_defs/line_marks.json",
+            convertData: convertForLineOrFill,
+            fillSpec: function(spec, dataset) {
+                spec.properties.update.stroke.value = dataset.color;
+            }
+        },
+        "fill": {
+            marksSpec: "./chart_defs/fill_marks.json",
+            convertData: convertForLineOrFill,
+            fillSpec: function(spec, dataset) {
+                spec.properties.update.fill.value = dataset.color;
+            }
+        },
+        "histogram": {
+            marksSpec: "./chart_defs/histogram_marks.json",
+            convertData: convertForIntensity,
+            fillSpec: function(spec, dataset) {
+                spec.properties.update.fill.value = dataset.color;
+            }
+        },
+        "intensity": {
+            marksSpec: "./chart_defs/intensity_marks.json",
+            convertData: convertForIntensity,
+            fillSpec: function(spec, dataset) {}
+        }
+    };
+
+    /**
+     * Draw a line chart with one or more data sources.
+     */
+    var drawLineChart = function(dataList, options) {
+        options = options || {};
+
+        chartAttrs = chartTypeDicts[options.type];
+
+        // Load chart definition, update, and display.
+        $.getJSON("./chart_defs/base.json", function(vizSpec) {
+            $.getJSON(chartAttrs.marksSpec, function(marksSpec) {
+
+                // Set chart attributes from options.
+                if (options.ydomain) {
+                    vizSpec.scales[1].domain = options.ydomain;
+                }
+
+                // Draw each dataset.
+                var yMax = 0;
+                for (var i = 0; i < dataList.length; i++) {
+                    var dataset = dataList[i],
+                        name = dataset.name,
+                        data = dataset.data,
+                        dataMarksSpec = $.extend(true, {}, marksSpec);
+
+                    // Convert features to datapoints. Each data point may be for a single
+                    // base or multiple bases.
+                    converted_data = chartAttrs.convertData(data);
+
+                    // Compute 90% quantile and set y max to that if its larger.
+                    dataSetMax = d3.quantile($.map(data, function(d) { return d.score; }), 0.95);
+                    if (dataSetMax > yMax) {
+                        yMax = dataSetMax;
+                    }
+
+                    dataMarksSpec.from.data = name;
+                    chartAttrs.fillSpec(dataMarksSpec, dataset);
+                    vizSpec.marks.push(dataMarksSpec);
+                    vizSpec.data.push({
+                        "name": name,
+                        "values": converted_data
+                    });
+                }
+
+                // Set yMax, multiplying by 1.5 to put maximum in middle of chart.
+                vizSpec.scales[1].domain = [0, yMax * 1.5];
+                vega.parse.spec(vizSpec, function(chart) {
+                    chart({el:"#vis"}).update();
+                });
+            });
+
+
         });
     };
 
